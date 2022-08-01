21e0cd2d2c5e0c54670d4f5fe24d4d8a2f9d3e3a
SODAAAAAAA <bsohee0206@gmail.com>
2022-08-01 18:30:03 +0900

    수정

---
+++ main.js
@@ -0,0 +1,11 @@
+let navBtn = document.querySelectorAll('.nav>li>a')
+let subNav = document.getElementsByClassName('sub_nav')
+let subBtn = document.querySelectorAll('.sub_nav li a')
+let subBg = document.getElementsByClassName('subnav')
+let header = document.getElementById('1')
+
+console.log(navBtn)
+console.log(subNav)
+console.log(subBtn)
+console.log(subBg)
+
